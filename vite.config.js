78bb597fe92f5a34ce2514a832be1e5f72c83681
dmjcomdem <d.romanov@zdravcity.ru>
2021-10-18 02:28:11 +0500

    feat: Добавление meta-tag в секцию head

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import svgr from 'vite-plugin-svgr'
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
     plugins: [reactRefresh(), tsconfigPaths(), svgr()]
